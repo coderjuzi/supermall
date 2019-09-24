22345bbe044ea3194a4c9531d5e535fd3752046c
Zihao Ju <vincent.juzihao@gmail.com>
2019-09-24 20:29:12 +0800

    推荐信息构建完成

---
+++ src/components/common/swiper/index.js
@@ -0,0 +1,6 @@
+import Swiper from './Swiper'
+import SwiperItem from './SwiperItem'
+
+export {// 将两个对象统一导出
+  Swiper, SwiperItem
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends"/>
   </div>
 </template>
 
 <script>
-  import NavBar from 'components/common/navbar/NavBar'
-  import {getHomeMultidata} from 'network/home'
+  import NavBar from 'components/common/navbar/NavBar';
+  import HomeSwiper from './childComps/HomeSwiper';
+  import RecommendView from './childComps/RecommendView';
+  import {getHomeMultidata} from 'network/home';
 
   export default {
     name: "Home",
     components: {
-      NavBar
+      NavBar,
+      HomeSwiper,
+      RecommendView
     },
     data() {
       return {
