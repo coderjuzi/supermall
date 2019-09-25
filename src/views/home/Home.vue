9d22dc8794547024c4363ed52bbd743079e11b25
Zihao Ju <vincent.juzihao@gmail.com>
2019-09-25 15:52:52 +0800

    实现首页数据的请求和保存

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -119,7 +119,7 @@
   import NavBar from 'components/common/navbar/NavBar';
   import TabControl from 'components/content/tabControl/TabControl';
   // 方法
-  import {getHomeMultidata} from 'network/home';
+  import {getHomeMultidata, getHomeGoods} from 'network/home';
 
   export default {
     name: "Home",
@@ -134,17 +134,39 @@
       return {
         // 组件中创建变量banners保存res，不会被内存回收
         banners: [],
-        recommends: []
+        recommends: [],
+        goods: {
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []}
+        }
       }
     },
     created() {// 使用生命周期函数，发送网络请求
       // 1. 请求多个数据
-      // 当.then()前的方法执行完后再执行then()内部的程序，这样就避免了数据没获取到的问题
-      getHomeMultidata().then(res => {// 函数执行完，会被内存回收
-        // 将数据res赋值给变量，存储在变量banners中，防止数据消失
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
-      })
+      this.getHomeMultidata()
+      // 2. 请求商品数据
+      this.getHomeGoods('pop')// 接收类型
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+    },
+    methods: {
+      getHomeMultidata() {
+        // 当.then()前的方法执行完后再执行then()内部的程序，这样就避免了数据没获取到的问题
+        getHomeMultidata().then(res => {// 函数执行完，会被内存回收
+          // 将数据res赋值给变量，存储在变量banners中，防止数据消失
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page + 1;// 根据类型拿到最新的页码
+        getHomeGoods(type, page).then(res => {// 请求最新的数据
+          // push(...nums)函数可以传入可变参数
+          this.goods[type].list.push(...res.data.list);// 根据类型拿到list，把最新拿到的数据塞入对应list里面
+          this.goods[type].page += 1;// 页码+1
+        })
+      }
     }
   }
 </script>
