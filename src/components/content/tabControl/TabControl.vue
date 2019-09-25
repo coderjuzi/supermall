b21264c2d755d8ce0576b688efb5b85a9e6a43b6
Zihao Ju <vincent.juzihao@gmail.com>
2019-09-25 13:42:12 +0800

    tabControl封装完成

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item, index) in titles"
+         class="tab-control-item" :class="{active: index === currentIndex}" @click="itemClick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabControl",
+    props: {
+      titles: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    data() {//用于改变高亮文字和下边框颜色
+      return {
+        currentIndex: 0//当前选中的索引
+      }
+    },
+    methods: {
+      itemClick(index) {
+        this.currentIndex = index;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-control {
+    display: flex;
+    text-align: center;
+    font-size: 15px;
+    height: 40px;
+    line-height: 40px;/*line-height可以被继承*/
+    background-color: #fff;
+  }
+
+  .tab-control-item {
+    flex: 1;/*距离平分*/
+  }
+
+  .tab-control-item span {
+    padding: 5px;
+  }
+
+  .active {
+    color: var(--color-high-text);/*高亮文字颜色*/
+  }
+
+  .active span {
+    border-bottom: 3px solid var(--color-tint);
+  }
+</style>
