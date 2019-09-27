bfeff1ee773c05b293077d2d79e457eb7a7e67a3
Zihao Ju <vincent.juzihao@gmail.com>
2019-09-27 22:03:29 +0800

    TabControl的吸顶效果修复完成

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <!--复制一份tab-control，属性设置为isTabFixed，默认为不可见-->
+    <tab-control :titles="['流行', '新款', '精选']"
+                 @tabClick="tabClick"
+                 ref="tabControl1"
+                 class="tab-control"
+                 v-show="isTabFixed"/>
     <!--传参时前面加上冒号则会将3/true当做数值型/布尔型传入，否则会被当做字符串-->
     <scroll class="content"
             ref="scroll"
@@ -8,13 +14,14 @@
             @scroll="contentScroll"
             :pull-up-load="true"
             @pullingUp="loadMore">
-      <home-swiper :banners="banners"/>
+      <!--使用子组件HomeSwiper中的swiperImageLoad方法监听图片的加载-->
+      <home-swiper :banners="banners" @swiperImageLoad="swiperImageLoad"/>
       <recommend-view :recommends="recommends"/>
       <feature-view/>
       <!--用 v-on 或 @ 语法糖，监听点击事件-->
-      <tab-control class="tab-control"
-                   :titles="['流行', '新款', '精选']"
-                   @tabClick="tabClick"/>
+      <tab-control :titles="['流行', '新款', '精选']"
+                   @tabClick="tabClick"
+                   ref="tabControl2"/>
       <goods-list :goods="showGoods"/>
     </scroll>
     <!--在监听一个组件的原生事件时，必须给对应事件加上.native修饰符-->
@@ -60,7 +67,9 @@
           'sell': {page: 0, list: []}
         },
         currentType: 'pop',// 默认当前类型（第一次展示的为pop）
-        isShowBackTop: false// 默认为：不显示
+        isShowBackTop: false,// 默认为：不显示
+        tabOffsetTop: 0,// offsetTop：当前对象到其上级层顶部的距离
+        isTabFixed: false// 默认：不吸顶
       }
     },
     computed: {// 使用计算属性代替goods[currentType].list（注意加.this）
@@ -79,7 +88,7 @@
     // created：在模板渲染成html前调用，即通常初始化某些属性值，然后再渲染成视图
     // mounted：在模板渲染成html后调用，通常是初始化页面完成后，再对html的dom节点进行一些需要的操作
     mounted() {// 使用mounted来确保scroll有值之后再刷新
-      // 3.监听item中图片加载完成的事件
+      // 1. item中图片加载完成的事件监听
       // 为避免refresh函数频繁调用，使用debounce防抖函数
       const refresh = debounce(this.$refs.scroll.refresh, 500)// 延迟半秒刷新
       this.$bus.$on('itemImageLoad', () => {
@@ -90,7 +99,7 @@
       /**
        * 事件监听相关的方法
        */
-      tabClick(index) {
+      tabClick(index) {// tabControl的点击监听
         switch (index) {
           case 0:
             this.currentType = 'pop'
@@ -102,16 +111,28 @@
             this.currentType = 'sell'
             break
         }
+        // 当点击之后，将两个tab-control的currentIndex统一为最新点击的index
+        this.$refs.tabControl1.currentIndex = index;
+        this.$refs.tabControl2.currentIndex = index;
       },
       backClick() {
         this.$refs.scroll.scrollTo(0, 0, 500)// 通过ref拿到对象，可以访问其内部属性和方法
       },
+      // 1. 判断BackTop是否显示
       contentScroll(position) {// 内容发生滚动时，可以拿到position
         // 将position的y值和1000作对比，当大于1000时显示BackTop图标
-        this.isShowBackTop = -position.y > 1000// y是一个负值，先转为正数
+        this.isShowBackTop = -(position.y) > 1000// y是一个负值，先转为正数
+        // 2. 决定tabControl是否吸顶
+        this.isTabFixed = (-position.y) > this.tabOffsetTop// 大于则吸顶
       },
       loadMore() {
         this.getHomeGoods(this.currentType)// 确定当前选中的栏目(type)
+      },
+      swiperImageLoad(){
+        // 获取tabControl的offsetTop
+        // 组件没有offsetTop属性，因此要拿到组件中的元素
+        // 所有的组件都有一个属性$el：用于获取组件中的元素
+        this.tabOffsetTop = this.$refs.tabControl2.$el.offsetTop
       },
       /**
        * 网络请求相关的方法
@@ -147,19 +168,24 @@
     background-color: var(--color-tint);
     color: #fff;
 
-    position: fixed;
-    left: 0;
-    right: 0;
-    top: 0;
-    z-index: 9;/*z-index属性指定一个元素的堆叠顺序，拥有更高堆叠顺序的元素总是会处于堆叠顺序较低的元素的前面*/
+    /*在使用浏览器原生滚动时，为了让导航不跟随一起滚动*/
+    /*现在使用better-scroll局部滚动，所以就不需要对导航进行fixed*/
+    /*position: fixed;!*元素的位置相对于浏览器窗口位置固定*!*/
+    /*left: 0;*/
+    /*right: 0;*/
+    /*top: 0;*/
+    /*z-index: 9;!*z-index属性指定一个元素的堆叠顺序，拥有更高堆叠顺序的元素总是会处于堆叠顺序较低的元素的前面*!*/
   }
 
-  .tab-control {
-    position: sticky;/*为tabControl添加吸顶效果，需要定义top属性*/
-    top: 44px;/*达到top值前position属性为sticky，达到top值后自动改为fixed*/
-    z-index: 9;/*防止图片遮挡住tabControl*/
-  }
-  /*确定中间滚动区域的高度*/
+  /*使用better-scroll后，此吸顶效果已失效*/
+  /*.tab-control {*/
+  /*  !*为tabControl添加吸顶效果，需要定义top属性*!*/
+  /*  position: sticky;*/
+  /*  top: 44px;!*达到top值前position属性为sticky，达到top值后自动改为fixed*!*/
+  /*  z-index: 9;!*防止图片遮挡住tabControl*!*/
+  /*}*/
+
+  /*确定中间局部滚动区域的高度*/
   /*方法一：定位*/
   .content {
     overflow: hidden;
@@ -175,4 +201,9 @@
   /*  overflow: hidden;*/
   /*  margin-top: 44px;*/
   /*}*/
+  .tab-control {
+    /*使用相对定位可以使得tabControl保持在原来的位置，而且可以设置z-index*/
+    position: relative;
+    z-index: 9;/*调整tabControl的堆叠顺序*/
+  }
 </style>
--- src/views/home/childComps/HomeSwiper.vue
+++ src/views/home/childComps/HomeSwiper.vue
@@ -3,7 +3,8 @@
     <swiper-item v-for="item in banners" >
       <!--动态绑定要加冒号-->
       <a :href="item.link">
-        <img :src="item.image" alt="">
+      <!--使用@load监听图片是否加载完毕，以确定offsetTop的距离-->
+        <img :src="item.image" alt="" @load="imageLoad">
       </a>
     </swiper-item>
   </swiper>
@@ -22,9 +23,23 @@
         }
       }
     },
+    data(){// 使用一个变量isLoad，来通知之前监听事件是否已经发出
+      return {
+        isLoad: false// 默认加载情况设为false
+      }
+    },
     components: {
       Swiper,
       SwiperItem
+    },
+    methods: {// 添加图片加载监听的方法
+      imageLoad() {
+        if (!this.isLoad){// 如果还没有加载
+          // 因为轮播图中有4张图片，所以监听射出去了4次事件
+          this.$emit('swiperImageLoad')// 只需要发出1次，因为只需要获取1个高度即可
+          this.isLoad = true// 此时将isLoad设置为true，使得循环只会执行一次
+        }
+      }
     }
   }
 </script>
