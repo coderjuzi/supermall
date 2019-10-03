7a9b64c52fccb73dc5f171897175ff88b12d6271
Zihao Ju <vincent.juzihao@gmail.com>
2019-10-03 18:58:40 +0800

    详情页-Vuex中代码的重构完成

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 
 import mutations from './mutations'
 import actions from './actions'
+import getters from './getters'
 
 // 1. 安装插件
 Vue.use(Vuex)
@@ -15,6 +16,7 @@ const store = new Vuex.Store({
   state,
   mutations,// 做跟踪
   actions,// 需要修改某个操作，其中有判断逻辑的，推荐使用actions
+  getters
 })
 // 3. 挂载到Vue实例上
 export default  store
--- src/views/shopcart/Shopcart.vue
+++ src/views/shopcart/Shopcart.vue
@@ -1,13 +1,38 @@
 <template>
-  <h2>购物车</h2>
+  <div class="cart">
+    <nav-bar class="nav-bar">
+      <div slot="center">购物车({{length}})</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+  import NavBar from 'components/common/navbar/NavBar'
+  import { mapGetters } from 'vuex'// 从vuex框架中导入
+
 export default {
-  name: 'Shopcart'
+  name: 'Shopcart',
+  components: {
+    NavBar
+  },
+  computed: {
+    // mapGetters辅助函数将store中的getter映射到局部计算属性
+    // 方法一：数组语法（使用原数组元素名）
+    // ...mapGetters(['cartLength', 'cartList'])//进行解构，将getters.js中的东西转换为计算属性
+
+    // 方法二：对象语法（可以自定义对象名）
+    ...mapGetters({
+      length: 'cartLength',
+      list: 'cartList'
+    })
+  }
 }
 </script>
 
 <style scoped>
-
+  .nav-bar {
+    background-color: var(--color-tint);
+    color: #fff;
+    /*font-weight: 700;!*加粗*!*/
+  }
 </style>
