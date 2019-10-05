a3e8a4d4e72150102c4fb2fb497784dde729a767
Zihao Ju <vincent.juzihao@gmail.com>
2019-10-05 19:27:33 +0800

    使用FastClick解决移动端300ms延迟问题

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import FastClick from 'fastclick'// 导入FastClick
+
 import toast from './components/common/toast'
 
 Vue.config.productionTip = false
@@ -10,6 +12,9 @@ Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()// prototype是全局属性，适用于所有的JS对象
 // 安装toast插件
 Vue.use(toast)// 使用use会执行obj的install函数（index.js中）
+
+// 解决移动端300毫秒延迟的问题
+FastClick.attach(document.body)
 
 new Vue({
   render: h => h(App),
